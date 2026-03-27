fa78ca013e581da66b017191487d3fa8154b1a74
Vincent <vincentliang0208@gmail.com>
2026-03-26 20:40:55 -0600

    implemented part of library and media manager

--- Media Tracker/src/main/java/org/compsci/ia/Library.java
+++ Media Tracker/src/main/java/org/compsci/ia/Library.java
@@ -1,7 +1,16 @@
 package org.compsci.ia;
 
+import java.util.ArrayList;
+
 /**
  * Stores all the media the user is tracking
  */
 public class Library {
+    private ArrayList<Media> medias;
+    public Library() {
+        medias = new ArrayList<>();
+    }
+    public ArrayList<Media> getMedias() {
+        return medias;
+    }
 }
--- Media Tracker/src/main/java/org/compsci/ia/MediaManager.java
+++ Media Tracker/src/main/java/org/compsci/ia/MediaManager.java
@@ -1,7 +1,35 @@
 package org.compsci.ia;
 
+import java.util.ArrayList;
+
 /**
  * Handles the storage and searching for the media tracker
  */
 public class MediaManager {
+    private Library library;
+
+    public MediaManager(Library library) {
+        this.library = library;
+    }
+
+    public void addMedia(Media media) {
+        library.getMedias().add(media);
+    }
+
+    public void removeMedia(Media media) {
+        library.getMedias().remove(media);
+    }
+
+    public ArrayList<Media> getAllMedia() {
+        return library.getMedias();
+    }
+
+    public void loadFromFile() {
+        //TODO
+    }
+
+    public void saveToFile() {
+        //TODO
+    }
+
 }
